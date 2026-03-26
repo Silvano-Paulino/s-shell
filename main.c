202f1946ea4798da165489bc11a1e8d0f135514c
silvano-paulino <silvanocavimbi20@gmail.com>
2026-03-26 16:42:44 +0100

    feat: implement handle_redirection from cmd contents

--- main.c
+++ main.c
@@ -5,13 +5,42 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdbool.h>
+#include <fcntl.h>
 
 #define MAX_ARGS 64
 #define DELIM " \t\r\n\a"
 
+#define MODE_APPEND 1
+#define MODE_TRUNCATE 2
+
 bool is_valid_token(char *token, int i)
 {
     return token != NULL && i < MAX_ARGS - 1;
+}
+
+char *handler_redirection(char **args, int *mode)
+{
+    for (int i = 0; args[i] != NULL; i++)
+    {
+        if (strcmp(args[i], ">") == 0)
+        {
+            char *filename = args[i + 1];
+            args[i] = NULL;
+
+            *mode = MODE_TRUNCATE;
+            return filename;
+        }
+
+        if (strcmp(args[i], ">>") == 0)
+        {
+            char *filename = args[i + 1];
+            args[i] = NULL;
+
+            *mode = MODE_APPEND;
+            return filename;
+        }
+    }
+    return NULL;
 }
 
 int main()
@@ -22,7 +51,7 @@ int main()
 
     while (1)
     {
-        printf("x-shell> ");
+        printf("s-shell> ");
 
         if (!fgets(line, sizeof(line), stdin))
             break;
@@ -41,17 +70,41 @@ int main()
         if (args[0] == NULL)
             continue;
 
-        __pid_t pid = fork();
+        __pid_t process_id = fork();
 
-        if (pid == 0)
+        if (process_id == 0)
         {
+            int mode = 0;
+            char *outFilename = handler_redirection(args, &mode);
+
+            if (outFilename)
+            {
+                int flags = (mode == MODE_APPEND) ? O_WRONLY | O_CREAT | O_APPEND : O_WRONLY | O_CREAT | O_TRUNC;
+
+                int file = open(outFilename, flags, 0644);
+
+                if (file < 0)
+                {
+                    perror("Error opening file for redirection");
+                    exit(EXIT_FAILURE);
+                }
+
+                if (dup2(file, STDOUT_FILENO) < 0)
+                {
+                    perror("Error redirecting output");
+                    exit(EXIT_FAILURE);
+                }
+
+                close(file);
+            }
+
             if (execvp(args[0], args) == -1)
             {
                 perror("Error executing command");
+                exit(EXIT_FAILURE);
             }
-            exit(EXIT_FAILURE);
         }
-        else if (pid < 0)
+        else if (process_id < 0)
         {
             perror("Error no process created");
             exit(EXIT_FAILURE);
